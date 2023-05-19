ba120ed0e2df19d1189d7855748eba872c1feaab
smnski <szymonurbanski217@gmail.com>
2023-05-19 21:33:44 +0200

    library finished

--- cwiczenie8/kolLib.h
+++ cwiczenie8/kolLib.h
@@ -1,7 +1,84 @@
 //Autor: Szymon Urbanski
 #include <iostream>
+#include <mqueue.h>
 
-void sayHello() {
-    
-    std::cout << "hi" << std::endl;
+mqd_t stworzMQ(const char* nazwa, struct mq_attr *attr) {
+
+    mqd_t des = open(nazwa, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
+    if(des == -1) {
+        perror("ERROR: open - stworzMQ - kolLib.h");
+        exit(1);
+    }
+
+return des;
+}
+
+mqd_t otworzMQ_Read(const char* nazwa) {
+
+    mqd_t des = open(nazwa, O_RDONLY);
+    if(des == -1) {
+        perror("ERROR: open - otworzMQ_Read - kolLib.h");
+        exit(1);
+    }
+
+return des;
+}
+
+mqd_t otworzMQ_Write(const char* nazwa) {
+
+    mqd_t des = open(nazwa, O_WRONLY);
+    if(des == -1) {
+        perror("ERROR: open - otworzMQ_Write - kolLib.h");
+        exit(1);
+    }
+
+return des;
+}
+
+void zamknijMQ(mqd_t des) {
+
+    if((mq_close(des)) == -1) {
+        perror("ERROR: mq_close - zamknijMQ - kolLib.h");
+        exit(1);
+    }
+}
+
+void usunMQ(const char* nazwa) {
+
+    if((mq_unlink(nazwa)) == -1) {
+        perror("ERROR: mq_unlink - usunMQ - kolLib.h");
+        exit(1);
+    }
+}
+
+void wyslijMQ(mqd_t des, const char* msg, int size, int prio) {
+
+    if((mq_send(des, msg, size, prio)) == -1) {
+        perror("ERROR: mq_send - wyslijMQ - kolLib.h");
+        exit(1);
+    }
+}
+
+void odbierzMQ(mqd_t des, char* msg, int size) {
+
+    if((mq_receive(des, msg, size, NULL)) == -1) {
+        perror("ERROR: mq_receive - odbierzMQ - kolLib.h");
+        exit(1);
+    }
+}
+
+void getAttr(mqd_t des, mq_attr* attr) {
+
+    if((mq_getattr(des, attr)) == -1) {
+        perror("ERROR: mq_getattr - getAttr - kolLib.h");
+        exit(1);
+    }
+}
+
+void setAttr(mqd_t des, mq_attr* oldAttr, mq_attr* newAttr) {
+
+    if((mq_setattr(des, newAttr, oldAttr)) == -1) {
+        perror("ERROR: mq_setattr - setAttr - kolLib.h");
+        exit(1);
+    }
 }
