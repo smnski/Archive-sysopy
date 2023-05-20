eb9592384ac2ef8194e1351ae90b4a08ff25eb5d
smnski <szymonurbanski217@gmail.com>
2023-05-20 22:48:06 +0200

    serwer boilerplate

--- cwiczenie8/kolLib.h
+++ cwiczenie8/kolLib.h
@@ -1,12 +1,31 @@
 //Autor: Szymon Urbanski
-#include <iostream>
 #include <mqueue.h>
+#include <ctime>
+
+//___________________________________________________//~Wspolne~//___________________________________________________//
+
+#define nazwa_MQ "/kolejka"
+#define size_MQ 150
+
+// Funkcja generujace liczbe pseudolosowa na podstawie obecnego czasu systemowego oraz losowej liczby.
+int losowaLiczba(int a, int b) {
+
+    // Ustawienie ziarna dla generatora.
+    std::srand(std::time(nullptr) + std::rand());
+
+    // Wygenerowanie losowej liczby z przedziału [a, b].
+    int num = std::rand() % (b - a + 1) + a;
+  
+    return num;
+}
+
+//___________________________________________________//~Biblioteka~//___________________________________________________//
 
 mqd_t stworzMQ(const char* nazwa, struct mq_attr *attr) {
 
-    mqd_t des = open(nazwa, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
+    mqd_t des = mq_open(nazwa, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
     if(des == -1) {
-        perror("ERROR: open - stworzMQ - kolLib.h");
+        perror("ERROR: mq_open - stworzMQ - kolLib.h");
         exit(1);
     }
 
@@ -15,9 +34,9 @@ return des;
 
 mqd_t otworzMQ_Read(const char* nazwa) {
 
-    mqd_t des = open(nazwa, O_RDONLY);
+    mqd_t des = mq_open(nazwa, O_RDONLY);
     if(des == -1) {
-        perror("ERROR: open - otworzMQ_Read - kolLib.h");
+        perror("ERROR: mq_open - otworzMQ_Read - kolLib.h");
         exit(1);
     }
 
@@ -26,9 +45,9 @@ return des;
 
 mqd_t otworzMQ_Write(const char* nazwa) {
 
-    mqd_t des = open(nazwa, O_WRONLY);
+    mqd_t des = mq_open(nazwa, O_WRONLY);
     if(des == -1) {
-        perror("ERROR: open - otworzMQ_Write - kolLib.h");
+        perror("ERROR: mq_open - otworzMQ_Write - kolLib.h");
         exit(1);
     }
 
--- cwiczenie8/serwer.cpp
+++ cwiczenie8/serwer.cpp
@@ -1,10 +1,69 @@
 //Autor: Szymon Urbanski
 #include <iostream>
-
+#include <signal.h>
 #include "kolLib.h"
+
+mqd_t des_serwera, des_klienta;
+
+void usunSerwerAtexit() {
+
+    zamknijMQ(des_serwera);
+    usunMQ(nazwa_MQ);
+
+    std::cout << "Zakonczenie programu - usuniecie serwera." << std::endl;
+}
+
+/*
+void usunSerwerSignal(int signum) {
+
+    zamknijMQ(des_serwera);
+    usunMQ(nazwa_MQ);
+    
+    std::cout << "Przerwanie programu - usuniecie serwera." << std::endl;
+}
+*/
+
+void wypiszAtrybuty(mqd_t des, mq_attr* attr) {
+
+    getAttr(des, attr);
+
+    std::cout << "Utworzono serwer o deskryptorze: " << des << ", atrybutach kolejki: " << std::endl;
+    std::cout << "  Nazwa: " << nazwa_MQ << std::endl;
+    std::cout << "  Flagi: " << attr->mq_flags << std::endl;
+    std::cout << "  Max. ilosc wiadomosci: " << attr->mq_maxmsg << std::endl;
+    std::cout << "  Max. rozmiar wiadomosci: " << attr->mq_msgsize << std::endl;
+}
 
 int main() {
 
-    
+    // Deklaracje zmiennych
+    char wiadomosc_wyslij[size_MQ];
+    char wiadomosc_odbierz[size_MQ];
+    char dzialanie;
+    int num1, num2;
+
+    pid_t clientID;
+
+    struct mq_attr atrybuty;
+
+    struct mq_attr creation_attr = {
+    .mq_flags = 0,
+    .mq_maxmsg = 15,
+    .mq_msgsize = size_MQ,
+    .mq_curmsgs = 0
+};
+
+    stworzMQ(nazwa_MQ, &atrybuty);
+    des_serwera = otworzMQ_Read(nazwa_MQ);
+
+    atexit(usunSerwerAtexit);
+
+    wypiszAtrybuty(des_serwera, &atrybuty);
+
+    while(!std::cin.eof()) {
+
+        break;
+
+    }
 
 }
